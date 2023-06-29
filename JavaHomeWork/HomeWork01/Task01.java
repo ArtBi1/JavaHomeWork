c1010638ca6002d3ca737c301728975b404f9203
ArtB <89324748589@mail.ru>
2023-06-30 03:13:28 +0500

    Решена задача 1

--- JavaHomeWork/HomeWork01/Task01.java
+++ JavaHomeWork/HomeWork01/Task01.java
@@ -1,5 +1,31 @@
-package JavaHomeWork.HomeWork01;
+import java.util.Scanner;
 
-public class Task01 {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Создаем объект Scanner для чтения ввода пользователя
+        System.out.print("Введите число n: ");
+        int n = scanner.nextInt();  // Считываем число N nextInt'ом
+        
+        int triangleNumber = calcNumber(n);  // Метод для вычисления треугольного числа
+        System.out.println("n-ое треугольное число: " + triangleNumber);
+        
+        long factorial = calcFactorial(n);  // Метод для вычисления факториала числа
+        System.out.println("Факториал числа n: " + factorial);
+    }
     
+    public static int calcNumber(int n) { // Метод для вычисления треугольного числа
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;  // Сумма + текущее число
+        }
+        return sum;  // Возвращаем готовую треугольную сумму
+    }
+    
+    public static long calcFactorial(int n) { // Метод для вычисления факториала
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;  // Множить текущее число на факториал
+        }
+        return factorial;  // Возвращаем готовый факториал
+    }
 }
